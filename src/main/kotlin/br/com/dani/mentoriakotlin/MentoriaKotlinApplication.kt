d91a8c69eae9467ea0560178528f795fe3a3c787
Danielle Campos <danielle.campos@picpay.com>
2022-08-31 20:01:15 -0300

    first commit

---
+++ src/main/kotlin/br/com/dani/mentoriakotlin/MentoriaKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dani.mentoriakotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MentoriaKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MentoriaKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dani/mentoriakotlin/MentoriaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dani.mentoriakotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MentoriaKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
